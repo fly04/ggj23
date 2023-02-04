eea950f0714a30e34a9ccde919ff2b1070f9aa8e
Luka Najjar <46554723+fly04@users.noreply.github.com>
2023-02-04 18:26:25 +0100

    Add fall stuff

---
+++ Assets/Tests/Game/FallZone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "FallHitbox")
+        {
+            GameObject parent = collision.gameObject.transform.parent.gameObject;
+            parent.GetComponent<HingeJoint2D>().enabled = false;
+            parent.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            parent.GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+            //collisons du container (pour qu'il tombe dedans)
+            parent.layer = 8;
+        }
+    }
+}
